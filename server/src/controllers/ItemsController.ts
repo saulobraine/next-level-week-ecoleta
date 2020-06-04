a2668b70898302f42d7a68a327483749cba5ea6b
Saulo Braine <saulobraine@outlook.com>
2020-06-03 21:52:35 -0300

    Aula 3 - A escolha da stack

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class ItemsController {
+
   async index(request: Request, response: Response) {
     const items = await knex('items').select('*');
 
@@ -15,6 +16,7 @@ class ItemsController {
 
     return response.json(serializedItems);
   }
+  
 }
 
 export default ItemsController;
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import logoBraine from './Logo-Braine.png';
 import './App.css';
+ 
+import Routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={logoBraine} className="App-Braine" />
-        <p>Agora vai!</p>
-      </header>
-    </div>
-  );
+
+  return <>
+    <Routes />
+  </>;
 }
 
 export default App;
